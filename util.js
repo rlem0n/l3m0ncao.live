699bbf76396acda526420320383740572f6b7dee
rlem0n <89490145+rlem0n@users.noreply.github.com>
2021-11-08 16:05:32 +1100

    Add files via upload

---
+++ util.js
@@ -0,0 +1,7 @@
+const discordInvite = "https://discord.gg/NyBg3dYXhu";
+console.log("why are you here?");
+document.getElementById("discordInviteBtn").setAttribute("href", discordInvite);
+particlesJS.load(
+    'particles-js', 
+    'particles.json', 
+);
